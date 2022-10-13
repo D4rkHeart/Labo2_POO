c3201bc2d52f46386b85217ed272bd1dd9bd38a5
D4rkHeart <leonardo.surdez@gmail.com>
2022-10-13 09:43:52 +0200

    [feat] In the bank class, the get summary function

--- Bank.cs
+++ Bank.cs
@@ -1,7 +1,7 @@
 class Bank 
 {
     public List<Customer> Customers = new List<Customer>();
-    public List<BankAccount> BankAccounts= new List<BankAccount>();
+    public List<BankAccount> BankAccounts = new List<BankAccount>();
 
     //Méthode qui enregistre un client 
     public Customer RegisterCustomer (String FirstName, String LastName)
@@ -17,7 +17,7 @@ class Bank
         return customer;
         
     }
-
+    //Méhode qui crée un compte pour un client
     public BankAccount CreateAccount (String FirstName, String LastName)
     {
         Customer Owner = this.RegisterCustomer(FirstName,LastName);
@@ -26,18 +26,38 @@ class Bank
         return account;
     }
 
-    // private Customer FindOrCreateCustomer (String Firstname, String LastName) 
-    // {
-    //     bool Found = false;
-    //     int NbCustomer = this.Customers.Length;
-    //     Customer customer;
-    //     for (int i;i=0; i<NbCustomer && !Found; i++)
-    //     {
-    //         customer = this.Customers[i];
-    //         Found = customer.FirstName == FirstName && customer.LastName == LastName; 
-    //     }
-    // }
-    // Customer? returnedCustomer = Customers.Find(CustomerMatcher(FirstName,LastName));
-    // private static Predicate<Customer> CustomerMatcher(String FirstName, String LastName) {
-    // return x => x.FirstName == FirstName && x.LastName == LastName;}
+    public BankAccount[] GetCustomerSummary (Customer customer)
+    {
+        Console.WriteLine("---------------");
+        Console.WriteLine(customer.FullName() + " :");
+        BankAccount[] CustomerAccounts = new BankAccount[CountBankAcount(customer)];
+        int AccountNumber = 0;
+        float TotalBalance = 0;
+        foreach (var account in this.BankAccounts)
+        {
+            if(account.Owner == customer)
+            {
+                CustomerAccounts[AccountNumber] = account;
+                AccountNumber += 1;
+                Console.WriteLine("- Compte #" + AccountNumber + ": " + account.Balance);
+                TotalBalance += account.Balance;
+            }
+        }
+        Console.WriteLine("Fortune totale : " + TotalBalance);
+        Console.WriteLine("---------------");
+        return CustomerAccounts;
+    }
+
+    public int CountBankAcount (Customer customer)
+    {
+        int CountAccount = 0;
+        foreach (var account in this.BankAccounts)
+        {
+            if(account.Owner == customer)
+            {
+                CountAccount += 1;
+            }
+        }
+        return CountAccount;
+    }
 }
